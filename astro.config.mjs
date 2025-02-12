2b24590846b370b9816a13c99152b86e64ce5b8d
Natalie Watkins <watkinsnbw@gmail.com>
2025-02-11 19:06:16 -0500

    updated url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aether-astro-theme.netlify.app/',
+  site: 'https://natwatkins.com',
   integrations: [tailwind(), react(), sitemap()],
 });
